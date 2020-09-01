bdbfcd2d4d07ae62806826950e2be49d5104a24a
casline <c_schafrath@yahoo.com>
2020-08-31 23:57:26 -0400

    Initial commit

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -7,7 +7,7 @@ const NavigationComponent = (props) => {
   const dynamicLink = (route, linkText) => {
     return (
       <div className="nav-link-wrapper">
-        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+        <NavLink to={route} activeClassName="nav-link-active">{linkText}</NavLink>
       </div>
     );
   };
@@ -41,8 +41,12 @@ const NavigationComponent = (props) => {
             <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
           </div>
 
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+          </div>
+
           {props.loggedInStatus === "LOGGED_IN" ? (
-            dynamicLink("/blog", "Blog") 
+            dynamicLink("/portfolio-manager", "Portfolio Manager") 
           ) : null}
         </div>
 
