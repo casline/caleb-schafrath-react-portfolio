a52f3b07a33d494867620dfd0530dd6e6e68b8a0
casline <c_schafrath@yahoo.com>
2020-09-07 23:34:00 -0400

    Initial commit

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -130,7 +130,7 @@ export default class PortfolioForm extends Component {
         formData.append("portfolio_item[position]", this.state.position);
 
         if (this.state.thumb_image) {
-          formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+            formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
         }
 
         if (this.state.banner_image) {
