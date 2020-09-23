714bfef83b7add6319e3709bf4eef497e8fc06a5
casline <c_schafrath@yahoo.com>
2020-09-23 18:43:31 -0400

    Initial commit

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
@@ -11,38 +10,38 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: []
+      data: [],
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-
   }
 
   handleFilter(filter) {
-      this.setState({
-          data: this.state.data.filter(item => {
-              return item.category === filter;
-          })
-      })
+    this.setState({
+      data: this.state.data.filter((item) => {
+        return item.category === filter;
+      }),
+    });
   }
 
   getPortfolioItems() {
-    axios.get('https://calebschafrath.devcamp.space/portfolio/portfolio_items')
-    .then(response => {
-      this.setState({
-        data: response.data.portfolio_items
+    axios
+      .get("https://calebschafrath.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
+        this.setState({
+          data: response.data.portfolio_items,
+        });
       })
-  })
-    .catch(error => {
-      console.log(error);
-  })
-    .finally(function () {
-    // always executed
-  });
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
   }
 
   portfolioItems() {
-    return this.state.data.map(item => {
+    return this.state.data.map((item) => {
       return <PortfolioItem key={item.id} item={item} />;
     });
   }
@@ -53,23 +52,23 @@ export default class PortfolioContainer extends Component {
 
   render() {
     if (this.state.isLoading) {
-        return <div>Loading...</div>;
+      return <div>Loading...</div>;
     }
 
     return (
-        <div className="portfolio-items-wrapper">
-          <button className="btn" onClick={() => this.handleFilter('eCommerce')}>
-            eCommerce
-          </button>
-          <button className="btn" onClick={() => this.handleFilter('Scheduling')}>
-            Scheduling
-          </button>
-          <button className="btn" onClick={() => this.handleFilter('Enterprise')}>
-            Enterprise
-          </button>
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-          {this.portfolioItems()}
-        </div>
+        {this.portfolioItems()}
+      </div>
     );
   }
 }
